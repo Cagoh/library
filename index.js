5a41b7afe97e4736b3a0463e53f7c3c6929a84c5
Blizz Softs <cagoh55@gmail.com>
2023-10-02 02:02:11 +0800

    updated library website

--- index.js
+++ index.js
@@ -19,19 +19,23 @@ const favDialog = document.getElementById("favDialog");
 const cancelButton = document.getElementById("cancel-button");
 const crossButton = document.getElementById("cross-button");
 
+const libraryContainer = document.getElementById("library-container");
+
+// Get the checkbox element by its ID
+var checkbox = document.getElementById("checkbox-read");
+
+
 // Add a click event listener to the "Cancel" button
 cancelButton.addEventListener("click", function () {
     // Close the dialog when the "Cancel" button is clicked
-    console.log("X!")
     favDialog.close();
 });
 
-// Add a click event listener to the "x" button
 crossButton.addEventListener("click", function () {
   // Close the dialog when the "Cancel" button is clicked
-  console.log("Cancel!")
   favDialog.close();
 });
+
 
 // Add a click event listener to the "Add" button
 addButton.addEventListener("click", function() {
@@ -46,6 +50,73 @@ addButton.addEventListener("click", function() {
   console.log("Pages:", pages);
 
   // You can also perform other actions, like adding the values to a list or sending them to a server.
+
+
+
+  // Create a new div element for bookContainer
+  const bookContainer = document.createElement("div");
+  // Set the id attribute for the created div element
+  bookContainer.id = "book-container";
+  
+  // create a div element to view library 
+  // Create a new paragraph element
+  const titleParagraph = document.createElement("p");
+  const authorParagraph = document.createElement("p");
+  const pagesParagraph = document.createElement("p");
+
+  // Create div element with id "checkbox-container"
+  const checkboxContainer = document.createElement("div");
+  checkboxContainer.id = "checkbox-container";
+
+  // Create Remove book button
+  const removeBookButton = document.createElement("button")
+  removeBookButton.textContent = "Remove book"
+
+  // Create input element
+  const checkboxInput = document.createElement("input");
+  checkboxInput.type = "checkbox";
+  checkboxInput.id = "checkbox-read";
+  checkboxInput.name = "book-status";
+  checkboxInput.value = "read";
+
+  // Create label element
+  const checkboxLabel = document.createElement("label");
+  checkboxLabel.htmlFor = "checkbox-read";
+  checkboxLabel.textContent = "Book Read";
+
+  // Check if the checkbox is checked
+  if (checkbox.checked) {
+    // Checkbox is checked
+    console.log("Checkbox is checked. Value: " + checkbox.value);
+    checkboxInput.checked = true;
+  } else {
+    // Checkbox is not checked
+    console.log("Checkbox is not checked.");
+    checkboxInput.checked = false;
+  }
+
+  // Append input and label elements to the checkboxContainer
+  checkboxContainer.appendChild(checkboxInput);
+  checkboxContainer.appendChild(checkboxLabel);
+
+  // Set the content of the paragraph element
+  titleParagraph.textContent = "Title: " + title;
+  authorParagraph.textContent = "Author:" + author;
+  pagesParagraph.textContent = "Total Pages: " + pages;
+
+  // Append the paragraph element to the bookContainer
+  bookContainer.appendChild(titleParagraph);
+  bookContainer.appendChild(authorParagraph);
+  bookContainer.appendChild(pagesParagraph);
+  bookContainer.appendChild(checkboxContainer);
+  bookContainer.appendChild(removeBookButton);
+
+  libraryContainer.appendChild(bookContainer);
+
+  removeBookButton.addEventListener("click", ()=>{
+    bookContainer.remove();
+  })
+
 });
 
 const addBookButton = document.getElementById("add-book-button")
@@ -54,11 +125,4 @@ const addBookButton = document.getElementById("add-book-button")
 addBookButton.addEventListener("click", ()=> {
   console.log("Add Book: clicked!")
   favDialog.showModal()
-})
-
-// favDialog.addEventListener("close", (e) => {
-//   outputBox.value =
-//     favDialog.returnValue === "default"
-//       ? "No return value."
-//       : `ReturnValue: ${favDialog.returnValue}.`; // Have to check for "default" rather than empty string
-// });
+});
